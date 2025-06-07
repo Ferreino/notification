7b3b466ebb7442c2f1f8b13d9d5cd75f5e82db0d
Fernando Reino <fsreino@sudamericano.edu.ec>
2025-06-06 21:50:01 -0500

    front3

--- src/app/notifications/notification.service.ts
+++ src/app/notifications/notification.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/envitonment.prod';
-
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
-  private apiUrl = 'http://localhost:3000/notifications';
+  private apiUrl = `${environment.apiUrl}/notifications`;
 
   constructor(private http: HttpClient) {}
 
@@ -25,5 +24,3 @@ export class NotificationService {
     return this.http.get<any[]>(this.apiUrl);
   }
 }
-
-
